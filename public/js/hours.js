45b49e675a4f37a52324e51d299a05a4dc5db73a
Albert Muela Dote <albertmueladote@gmail.com>
2024-11-29 20:16:48 +0100

    fix total hours

--- public/js/hours.js
+++ public/js/hours.js
@@ -21,7 +21,7 @@ $( document ).ready(function() {
      			if(hours > 0) {
 	        		addEvent(calendar, hours, info.dateStr);
 	        	}
-	        	totalHours(calendar, info.dateStr);
+	        	totalWeekHours(calendar, info.dateStr);
 	        	saveHour(hours, info.dateStr);
 	        } else {
 	            alert("Por favor, introduce un número válido.");
@@ -32,7 +32,7 @@ $( document ).ready(function() {
 	    	totalColumn();
 	    	var events = calendar.getEvents();
             events.forEach(function(event) {
-                totalHours(calendar, event.startStr);
+                totalWeekHours(calendar, event.startStr);
             });
 	    	loading();
 	    }
@@ -71,7 +71,7 @@ function totalColumn()
 	});
 }
 
-function totalHours(calendar, dateStr)
+function totalWeekHours(calendar, dateStr)
 {
 	var clickedDate = new Date(dateStr);
 	if(clickedDate.getDay() == 0) {
@@ -82,8 +82,8 @@ function totalHours(calendar, dateStr)
     var startOfWeek = new Date(clickedDate.setDate(clickedDate.getDate() - getDay + 1));
     var endOfWeek = new Date(clickedDate.setDate(clickedDate.getDate() + 6));
 
-    var startOfWeekStr = startOfWeek.toISOString().split('T')[0];
-    var endOfWeekStr = endOfWeek.toISOString().split('T')[0];
+    var startOfWeekStr = formatDateLocal(startOfWeek);
+    var endOfWeekStr = formatDateLocal(endOfWeek);
 
     var eventsInWeek = [];
 	var events = calendar.getEvents();
@@ -99,10 +99,6 @@ function totalHours(calendar, dateStr)
 	        var minutes = parseInt(parts[1], 10) || 0; // Minutos
 
 	        totalMinutes += hours * 60 + minutes; 
- 
-	         /*if(totalTd == undefined) {
-	         	totalTd = $('td[data-date="' + dateStr + '"]').closest('tr').find('td.fc-total .fc-daygrid-day-total');
-	         }*/
 	    }
 	});
 
@@ -125,6 +121,51 @@ function totalHours(calendar, dateStr)
     var totalTd = $('td[data-date="' + endOfWeekStr + '"]').closest('tr').find('td.fc-total .fc-daygrid-day-total');
     $('td[data-date="' + startOfWeekStr + '"]').closest('tr').find('td.fc-total .fc-daygrid-day-total').html('');
     $(totalTd).html(resultHtml);
+    totalMonthHours(calendar, dateStr);
+}
+
+function totalMonthHours(calendar, dateStr)
+{
+	var clickedDate = new Date(dateStr);
+
+    var startOfMonth = new Date(clickedDate.getFullYear(), clickedDate.getMonth(), 1);
+    var endOfMonth = new Date(clickedDate.getFullYear(), clickedDate.getMonth() + 1, 0);
+
+    var startOfMonthStr = formatDateLocal(startOfMonth);
+    var endOfMonthStr = formatDateLocal(endOfMonth);
+
+    var totalMinutes = 0;
+
+    var events = calendar.getEvents();
+    events.forEach(function (event) {
+        var eventDate = event.startStr;
+
+        if (eventDate >= startOfMonthStr && eventDate <= endOfMonthStr) {
+            var parts = event.title.split('.');
+            var hours = parseInt(parts[0], 10) || 0; // Horas
+            var minutes = parseInt(parts[1], 10) || 0; // Minutos
+
+            totalMinutes += hours * 60 + minutes;
+        }
+    });
+
+    var totalHours = Math.floor(totalMinutes / 60);
+    var remainingMinutes = totalMinutes % 60;
+
+    var total = totalHours + '.' + (remainingMinutes < 10 ? '0' + remainingMinutes : remainingMinutes);
+
+    if($('td[data-date="' + startOfMonthStr + '"]').closest('.fc-multimonth-month').find('.total-hours').length == 0) {
+    	$('td[data-date="' + startOfMonthStr + '"]').closest('.fc-multimonth-month').find('.fc-multimonth-header .fc-multimonth-title').after("<div class='total-hours'>(" + total + ")</div>");
+    } else {
+    	$('td[data-date="' + startOfMonthStr + '"]').closest('.fc-multimonth-month').find('.total-hours').html("(" + total + ")");
+    }
+}
+
+function formatDateLocal(date) {
+    var year = date.getFullYear();
+    var month = (date.getMonth() + 1).toString().padStart(2, '0'); // Mes empieza en 0
+    var day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
 }
 
 function saveHour(hours, date)
@@ -161,7 +202,7 @@ function getHours(calendar)
         success: function(response) {
         	JSON.parse(response).hours.forEach(function(eventData) {
 	            addEvent(calendar, eventData.hours, eventData.date);
-	            totalHours(calendar, eventData.date);
+	            totalWeekHours(calendar, eventData.date);
 	        });
         },
         error: function(xhr, status, error) {
